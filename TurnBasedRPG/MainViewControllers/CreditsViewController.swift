5cd9091445c82deba8f4dde8e9b77248c42030d5
Grymm315 <chris.phillips08@gmail.com>
2024-01-21 17:13:56 -0600

    Roll Credits

---
+++ TurnBasedRPG/MainViewControllers/CreditsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  TurnBasedRPG
+//
+//  Created by Christopher Phillips on 8/26/18.
+//  Copyright © 2018 Chris Phillips. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+    
+
+    @IBOutlet var creditView: UIButtonGUI!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        creditView.fromBottom()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
