0808854a1910d5629e9d2cd22327b7550b497d2b
Grymm315 <chris.phillips08@gmail.com>
2024-01-25 02:57:03 -0600

    Dialog

--- TurnBasedRPG/MainViewControllers/NPCDialogViewController.swift
+++ TurnBasedRPG/MainViewControllers/NPCDialogViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class NPCDialogViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var npcImage: UIImageView!
     @IBOutlet weak var dialogList: UITableView!
+    @IBOutlet weak var npcSpeechBubble: UILabel!
+    
     var dialogOptions:[String] = [
-    "Does the train stop near here?",
+    "OK- So your not going to believe me but I see subtitles when people talk and I'm just reading the subtitles. Seriously.",
     "Pardon me, I'm looking for some cream",
     "I don't know what to do with myself anymore. It feels like I've been all over the world but nothing seems to make me happy."]
     
@@ -20,6 +22,12 @@ class NPCDialogViewController: UIViewController, UITableViewDelegate, UITableVie
         super.viewDidLoad()
         dialogList.delegate = self
         dialogList.dataSource = self
+        npcSpeechBubble.layer.borderWidth = 2
+        npcSpeechBubble.layer.borderColor = UIColor.white.cgColor
+        npcSpeechBubble.layer.cornerRadius = 10
+        npcSpeechBubble.layer.masksToBounds = true
+        
+        npcSpeechBubble.text = "  Cindy: What are you looking at? Do I have something on my shirt?"
        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,6 +38,16 @@ class NPCDialogViewController: UIViewController, UITableViewDelegate, UITableVie
         let cell = tableView.dequeueReusableCell(withIdentifier: "dialogCell", for: indexPath) as! DialogCell
         cell.configCell(dialogOptions[indexPath.row])
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (indexPath.row == 0) {
+            npcSpeechBubble.text = "Oh. Teeheehee. Sorry there are a bunch of creepos out there. Did you know that you can tap on text bubbles and wierd things might happen? Oh. Teeheehee. Sorry there are a bunch of creepos out there. Did you know that you can tap on text bubbles and wierd things might happen?Oh. Teeheehee. Sorry there are a bunch of creepos out there. Did you know that you can tap on text bubbles and wierd things might happen?"
+        } else if (indexPath.row == 1) {
+            npcSpeechBubble.text = "You can check over at Millie's general store. I think she said she was bottling man milk. It's milk for men."
+        } else if (indexPath.row == 2) {
+            npcSpeechBubble.text = "Unfortunately there is a finite game world at this time. Please help contribute to the making of this game."
+        }
     }
     
 
